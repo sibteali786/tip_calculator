609a6434555ea54faff629ab70f97da53362e1d6
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2021-10-18 17:51:33 +0500

    added selectSubtotal and selectTipAmount

--- src/store/items/selector.js
+++ src/store/items/selector.js
@@ -2,3 +2,12 @@ import { createSelector } from 'reselect';
 
 export const selectItems = (state) => state.items;
 export const selectTipPercentage = (state) => state.tipPercentage;
+
+export const selectSubtotal = createSelector([selectItems], (items) =>
+  items.reduce((sum, item) => sum + item.price * item.quantity, 0)
+);
+
+export const selectTipAmount = createSelector(
+  [selectSubtotal, selectTipPercentage],
+  (subtotal, tipPercentage) => subtotal + tipPercentage
+);
